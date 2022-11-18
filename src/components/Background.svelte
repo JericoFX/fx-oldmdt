8256d04f4b9cec8f3844144097f3d1b70e3dde3f
JericoFX <56100605+JericoFX@users.noreply.github.com>
2022-11-18 16:14:17 -0300

    Screen component

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	export const size = '';
+/**
+* Theme: Choose the color of the background
+*/
 	export let theme: 'blue-black' | 'blue-white' | 'green-black' | 'green-white' | 'cyan-black' | 'cyan-white' | 'red-black' | 'red-white' | 'purple-black' | 'purple-white' | 'yellow-black' | 'yellow-white' | 'orange-black' | 'orange-white' = 'blue-black';
 </script>
 
---
+++ src/components/Screen.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+/**
+* Small set the screen 640 x 480
+*/
+export let small = false
+/**
+* Medium set the screen 800 x 600
+*/
+export let medium = true
+/**
+* Large set the screen 1024 x 768
+*/
+export let large = false
+/**
+* Border: Set a small padding around the box
+*/
+export let border = false
+/**
+* Center: Center the window 
+*/
+export let center = false
+
+let sm = small ? "tui-screen-640-480" : ""
+let md = medium ? "tui-screen-800-600" : ""
+let lg = large ? "tui-screen-1024-768" : ""
+let bordered= border ? "bordered" : ""
+let cnt = center ? "centered":""
+</script>
+
+<div class={`${sm} ${md} ${lg} ${bordered} ${cnt} ${$$props.class}`}>
+  <slot/>
+</div>
